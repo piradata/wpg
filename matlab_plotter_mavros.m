bc62037d77b44ba80f3a5b76342ff0a6ce5b7156
piradata <guilhermefercar@gmail.com>
2022-04-17 22:04:42 -0300

    add plot of error graph and autosave plots on graph folder

--- matlab_plotter_mavros.m
+++ matlab_plotter_mavros.m
@@ -1,9 +1,10 @@
 Controllers = ["SMC", "PID"];
+graph_folder = "graps";
 for i = 1:length(Controllers)
 
     Controller = Controllers(i);
     
-    BAG = rosbag([sprintf("%s_test_supernew.bag", Controller)]);
+    BAG = rosbag(sprintf("%s_test.bag", Controller));
     
     titulo = sprintf("teste com controlador %s", Controller);
     
@@ -59,93 +60,223 @@ for i = 1:length(Controllers)
     refyPoints = cellfun(@(m) double(m.Twist.Linear.Y),data_ref_vel);
     
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    figure('Name', Controller)
-    subplot(2, 3, 1)
-    xlabel('x-axis')
-    xlim([0 inf]) 
-    plot(ptime - BAG.StartTime, xPoints,'DisplayName','X position');
-    hold on
-    plot(ptimeRef - BAG.StartTime, xPointsRef,'DisplayName','Y ref position');
-    title('Posição X')
-    hold off
-    legend
-    
+    FigName = strcat(Controller, "_", "all");
+    all_fig = figure('Name', FigName,'Position', get(0, 'Screensize'));
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    subplot(2, 3, 2)
-    xlabel('y-axis')
-    xlim([0 inf]) 
+    pos_x = subplot(3, 3, 1);
+    xlabel('x-axis');
+    xlim([0 inf]);
+    plot(ptime - BAG.StartTime, xPoints,'DisplayName','X position');
+    hold on;
+    plot(ptimeRef - BAG.StartTime, xPointsRef,'DisplayName','X ref position');
+    title('Position X');
+    hold off;
+    legend;
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    pos_y = subplot(3, 3, 2);
+    xlabel('y-axis');
+    xlim([0 inf]);
     plot(ptime - BAG.StartTime, yPoints,'DisplayName','Y position');
     hold on
     plot(ptimeRef - BAG.StartTime, yPointsRef,'DisplayName','Y ref position');
-    title('Posição Y')
+    title('Position Y');
     hold off
     legend
-    
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    subplot(2, 3, 3)
-    xlabel('z-axis')
-    xlim([0 inf]) 
+    pos_z = subplot(3, 3, 3);
+    xlabel('z-axis');
+    xlim([0 inf]);
     plot(ptime - BAG.StartTime, zPoints,'DisplayName','Z position');
-    hold on
+    hold on;
     plot(ptimeRef - BAG.StartTime, zPointsRef,'DisplayName','Z ref position');
-    title('Posição Z')
-    hold off
-    legend
-    
+    title('Position Z');
+    hold off;
+    legend;
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    
-    subplot(2, 3, 4)
+    vel_x = subplot(3, 3, 4);
     plot(veltime - BAG.StartTime, xvelPoints,'DisplayName','X Velocity');
-    hold on
+    hold on;
     plot(velreftime - BAG.StartTime, xvelrefPoints,'DisplayName','X Velocity REF');
-    xlim([0 inf]) 
-    title('Xvel')
-    hold off
-    legend
-    
+    xlim([0 inf]);
+    title('Velocity X');
+    hold off;
+    legend;
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    
-    subplot(2, 3, 5)
+    vel_y = subplot(3, 3, 5);
     plot(veltime - BAG.StartTime, yvelPoints,'DisplayName','Y Velocity');
-    hold on
+    hold on;
     plot(velreftime - BAG.StartTime, yvelrefPoints,'DisplayName','Y Velocity REF');
-    xlim([0 inf]) 
-    title('Yvel')
-    hold off
-    legend
-    
+    xlim([0 inf]);
+    title('Velocity Y');
+    hold off;
+    legend;
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    
-    subplot(2, 3, 6)
+    vel_z = subplot(3, 3, 6);
     plot(veltime - BAG.StartTime, zvelPoints,'DisplayName','Z Velocity');
-    hold on
+    hold on;
     plot(velreftime - BAG.StartTime, zvelrefPoints,'DisplayName','Z Velocity REF');
-    xlim([0 inf])
-    title('Zvel')
-    hold off
-    legend
-    
+    xlim([0 inf]);
+    title('Velocity Z');
+    hold off;
+    legend;
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    figure('Name', Controller)
-    plot3(xPoints,yPoints,zPoints,'DisplayName', 'real movement')
-    hold on
-    plot3(xPointsRef,yPointsRef,zPointsRef,'DisplayName', 'wanted movement')
-    xlabel('X')
-    ylabel('Y')
-    zlabel('Z')
-    title(sprintf('Position tracker long distance %s', Controller))
-    hold off
+    err_pos_x = subplot(3, 3, 7);
+    xlabel('x-axis');
+    xlim([0 inf]);
+    time = ptime - BAG.StartTime;
+    stime = time(1);
+    ftime = time(end);
+    sourceSize = size(xPointsRef);
+    t = linspace(stime,ftime,sourceSize(1));
+    targetSize = size(xPoints);
+    ti = linspace(stime,ftime,targetSize(1));
+    xPointsRef_interpolated = interp1(t,xPointsRef,ti)';
+    plot(ptime - BAG.StartTime, xPoints - xPointsRef_interpolated,'DisplayName','X position error');
+    title('Position error X');
+    legend;
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    err_pos_y = subplot(3, 3, 8);
+    xlabel('y-axis');
+    xlim([0 inf]);
+    time = ptime - BAG.StartTime;
+    stime = time(1);
+    ftime = time(end);
+    sourceSize = size(yPointsRef);
+    t = linspace(stime,ftime,sourceSize(1));
+    targetSize = size(yPoints);
+    ti = linspace(stime,ftime,targetSize(1));
+    yPointsRef_interpolated = interp1(t,yPointsRef,ti)';
+    plot(ptime - BAG.StartTime, yPoints - yPointsRef_interpolated,'DisplayName','Y position error');
+    title('Position error Y');
     legend
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    err_pos_z = subplot(3, 3, 9);
+    xlabel('z-axis');
+    xlim([0 inf]);
+    time = ptime - BAG.StartTime;
+    stime = time(1);
+    ftime = time(end);
+    sourceSize = size(zPointsRef);
+    t = linspace(stime,ftime,sourceSize(1));
+    targetSize = size(zPoints);
+    ti = linspace(stime,ftime,targetSize(1));
+    zPointsRef_interpolated = interp1(t,zPointsRef,ti)';
+    plot(ptime - BAG.StartTime, zPoints - zPointsRef_interpolated,'DisplayName','Z position error');
+    title('Position error Z');
+    legend;
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    set(all_fig, 'PaperPositionMode', 'auto');
+    exportgraphics(all_fig, strcat(fullfile(graph_folder, FigName), ".png"), 'Resolution', 900);
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    hfig = figure;
+    hax_new = copyobj(pos_x, hfig);
+    set(hax_new, 'Position', get(0, 'DefaultAxesPosition'));
+    set(hfig, 'PaperPositionMode', 'auto');
+    legend(hax_new, 'Location', 'southeast');
+    xlabel('Time[s]');
+    ylabel('Position[m]');
+    exportgraphics(hfig, strcat(fullfile(graph_folder, strcat(Controller, "_posx")), ".png"), 'Resolution', 600);
+
+    hfig = figure;
+    hax_new = copyobj(pos_y, hfig);
+    set(hax_new, 'Position', get(0, 'DefaultAxesPosition'));
+    set(hfig, 'PaperPositionMode', 'auto');
+    legend(hax_new, 'Location', 'southeast');
+    xlabel('Time[s]');
+    ylabel('Position[m]');
+    exportgraphics(hfig, strcat(fullfile(graph_folder, strcat(Controller, "_posy")), ".png"), 'Resolution', 600);
+
+    hfig = figure;
+    hax_new = copyobj(pos_z, hfig);
+    set(hax_new, 'Position', get(0, 'DefaultAxesPosition'));
+    set(hfig, 'PaperPositionMode', 'auto');
+    legend(hax_new, 'Location', 'northwest');
+    xlabel('Time[s]');
+    ylabel('Position[m]');
+    exportgraphics(hfig, strcat(fullfile(graph_folder, strcat(Controller, "_posz")), ".png"), 'Resolution', 600);
+
+    hfig = figure;
+    hax_new = copyobj(vel_x, hfig);
+    set(hax_new, 'Position', get(0, 'DefaultAxesPosition'));
+    set(hfig, 'PaperPositionMode', 'auto');
+    legend(hax_new, 'Location', 'northwest');
+    xlabel('Time[s]');
+    ylabel('Velocity[m/s]');
+    exportgraphics(hfig, strcat(fullfile(graph_folder, strcat(Controller, "_velx")), ".png"), 'Resolution', 600);
+
+    hfig = figure;
+    hax_new = copyobj(vel_y, hfig);
+    set(hax_new, 'Position', get(0, 'DefaultAxesPosition'));
+    set(hfig, 'PaperPositionMode', 'auto');
+    legend(hax_new, 'Location', 'northwest');
+    xlabel('Time[s]');
+    ylabel('Velocity[m/s]');
+    exportgraphics(hfig, strcat(fullfile(graph_folder, strcat(Controller, "_vely")), ".png"), 'Resolution', 600);
+
+    hfig = figure;
+    hax_new = copyobj(vel_z, hfig);
+    set(hax_new, 'Position', get(0, 'DefaultAxesPosition'));
+    set(hfig, 'PaperPositionMode', 'auto');
+    legend(hax_new, 'Location', 'northwest');
+    xlabel('Time[s]');
+    ylabel('Velocity[m/s]');
+    exportgraphics(hfig, strcat(fullfile(graph_folder, strcat(Controller, "_velz")), ".png"), 'Resolution', 600);
+
+    hfig = figure;
+    hax_new = copyobj(err_pos_x, hfig);
+    set(hax_new, 'Position', get(0, 'DefaultAxesPosition'));
+    set(hfig, 'PaperPositionMode', 'auto');
+    legend(hax_new, 'Location', 'southwest');
+    xlabel('Time[s]');
+    ylabel('Position Error[m]');
+    exportgraphics(hfig, strcat(fullfile(graph_folder, strcat(Controller, "_poserrx")), ".png"), 'Resolution', 600);
+
+    hfig = figure;
+    hax_new = copyobj(err_pos_y, hfig);
+    set(hax_new, 'Position', get(0, 'DefaultAxesPosition'));
+    set(hfig, 'PaperPositionMode', 'auto');
+    legend(hax_new, 'Location', 'southwest');
+    xlabel('Time[s]');
+    ylabel('Position Error[m]');
+    exportgraphics(hfig, strcat(fullfile(graph_folder, strcat(Controller, "_poserry")), ".png"), 'Resolution', 600);
+
+    hfig = figure;
+    hax_new = copyobj(err_pos_z, hfig);
+    set(hax_new, 'Position', get(0, 'DefaultAxesPosition'));
+    set(hfig, 'PaperPositionMode', 'auto');
+    legend(hax_new, 'Location', 'northwest');
+    xlabel('Time[s]');
+    ylabel('Position Error[m]');
+    exportgraphics(hfig, strcat(fullfile(graph_folder, strcat(Controller, "_poserrz")), ".png"), 'Resolution', 600);
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    FigName = strcat(Controller, "_", "flower");
     
-%     figure('Name', Controller)
-%     plot3(xPoints,yPoints,zPoints,'DisplayName', 'real movement')
-%     hold on
-%     plot3(xPointsRef,yPointsRef,zPointsRef,'DisplayName', 'wanted movement')
-%     axis([-3, 3, -3, 3, 0, 6])
-%     xlabel('X')
-%     ylabel('Y')
-%     zlabel('Z')
-%     title(sprintf('Position tracker short distance %s', Controller))
-%     hold off
-%     legend
+    flower_fig = figure('Name', FigName);
+    plot3(xPoints,yPoints,zPoints,'DisplayName', 'Real movement');
+    hold on;
+    plot3(xPointsRef,yPointsRef,zPointsRef,'DisplayName', 'Desired movement');
+    xlabel('X position[m]');
+    xh = get(flower_fig.CurrentAxes,'XLabel'); % Handle of the x label
+    set(xh, 'Units', 'Normalized');
+    pos = get(xh, 'Position');
+    set(xh, 'Position',pos.*[1,1,1],'Rotation',22);
+    xtickangle(-30);
+    ylabel('Y position[m]');
+    yh = get(flower_fig.CurrentAxes,'YLabel'); % Handle of the y label
+    set(yh, 'Units', 'Normalized');
+    pos = get(yh, 'Position');
+    set(yh, 'Position',pos.*[1,1,1],'Rotation',-22);
+    ytickangle(30);
+    zlabel('Z position[m]');
+    xticks(-2:0.5:2);
+    yticks(-2:0.5:2);
+    zticks(0:0.5:4);
+    title(sprintf('Position tracker long distance %s', Controller));
+    hold off;
+    legend('Location', 'northeast');
+
+    set(flower_fig, 'PaperPositionMode', 'auto');
+    exportgraphics(flower_fig, strcat(fullfile(graph_folder, FigName), ".png"), 'Resolution', 900);
+
 end
+close all
