a136317f6d3447e25432bce7598d99dc864f70e1
Guilherme Ferreira <guilherme.ferreira@visagio.com>
2022-06-28 20:40:07 -0300

    update plotter to consider 3 paths

--- matlab_plotter_mavros_v2.m
+++ matlab_plotter_mavros_v2.m
@@ -1,4 +1,4 @@
-Controllers = ["SMC", "PID"];
+Controllers = ["SMC", "PID", "SMC_without_wind", "PID_without_wind"];
 graph_folder = "graps";
 xPoints = {};
 yPoints = {};
@@ -128,11 +128,13 @@ for i = 1:length(Controllers)
 end
 
 FigName = strcat("path_complete");
-flower_complete_fig = figure('Name', FigName, 'Position', get(0, 'Screensize'));
-plot3(xPointsRef{i},yPointsRef{i},zPointsRef{i},'DisplayName', 'Desired movement');
+path_complete_fig = figure('Name', FigName, 'Position', get(0, 'Screensize'));
+plot3(xPointsRef{1},yPointsRef{1},zPointsRef{1},'DisplayName', 'Desired movement');
 hold on
-plot3(xPoints{1},yPoints{1},zPoints{1},'DisplayName', 'Real movement SMC');
-plot3(xPoints{2},yPoints{2},zPoints{2},'DisplayName', 'Real movement PID');
+plot3(xPoints{1},yPoints{1},zPoints{1},'DisplayName', 'Real movement SMC with wind');
+plot3(xPoints{2},yPoints{2},zPoints{2},'DisplayName', 'Real movement PID with wind');
+plot3(xPoints{3},yPoints{3},zPoints{3},'DisplayName', 'Real movement SMC without wind');
+plot3(xPoints{4},yPoints{4},zPoints{4},'DisplayName', 'Real movement PID without wind');
 title('Position tracker');
 hold off;
 
@@ -140,74 +142,69 @@ xlabel('Absolute Position x[m]');
 ylabel('Absolute Position Y[m]')
 zlabel('Absolute Position Z[m]')
 legend('Location', 'northeast');
-set(flower_complete_fig, 'PaperPositionMode', 'auto');
+set(path_complete_fig, 'PaperPositionMode', 'auto');
 exportgraphics(flower_complete_fig, strcat(fullfile(graph_folder, FigName), ".png"), 'Resolution', 900);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 FigName = "all_fig";
 all_fig = figure('Name', FigName, 'Position', get(0, 'Screensize'));
-
-pos_x = subplot(3, 1, 1);
-time_x_1 = ptime{1} - BAG{1}.StartTime;
-points_x_1= xPoints{1} - xPointsRef_interpolated{1};
-idx1 = find(~isnan(points_x_1), 1);
-first_valid_time_x_1 = time_x_1(idx1);
-time_x_2 = ptime{2} - BAG{2}.StartTime;
-points_x_2 = xPoints{2} - xPointsRef_interpolated{2};
-idx2 = find(~isnan(points_x_2), 1);
-first_valid_time_x_2 = time_x_2(idx2);
-
-plot(time_x_1 - first_valid_time_x_1, xPointsRef_interpolated{1} - xPoints{1},'DisplayName','SMC position error');
-hold on
-plot(time_x_2 - first_valid_time_x_2, xPointsRef_interpolated{2} - xPoints{2},'DisplayName','PID position error');
-hold off
-xlim([0 inf]);
-xlabel('Time[s]');
-ylabel('Distance Error[m]')
-title('Position error X');
-legend
-
-pos_y = subplot(3, 1, 2);
-time_y_1 = ptime{1} - BAG{1}.StartTime;
-points_y_1= yPoints{1} - yPointsRef_interpolated{1};
-idx1 = find(~isnan(points_y_1), 1);
-first_valid_time_y_1 = time_y_1(idx1);
-time_y_2 = ptime{2} - BAG{2}.StartTime;
-points_y_2 = yPoints{2} - yPointsRef_interpolated{2};
-idx2 = find(~isnan(points_y_2), 1);
-first_valid_time_y_2 = time_y_2(idx2);
-
-plot(time_y_1 - first_valid_time_y_1, yPointsRef_interpolated{1} - yPoints{1},'DisplayName','SMC position error');
-hold on
-plot(time_y_2 - first_valid_time_y_2, yPointsRef_interpolated{2} - yPoints{2},'DisplayName','PID position error');
-hold off
-xlim([0 inf]);
-xlabel('Time[s]');
-ylabel('Distance Error[m]')
-title('Position error Y');
-legend
-
-pos_z = subplot(3, 1, 3);
-time_z_1 = ptime{1} - BAG{1}.StartTime;
-points_z_1= zPoints{1} - zPointsRef_interpolated{1};
-idx1 = find(~isnan(points_z_1), 1);
-first_valid_time_z_1 = time_z_1(idx1);
-time_z_2 = ptime{2} - BAG{2}.StartTime;
-points_z_2 = zPoints{2} - zPointsRef_interpolated{2};
-idx2 = find(~isnan(points_z_2), 1);
-first_valid_time_z_2 = time_z_2(idx2);
-
-plot(time_z_1 - first_valid_time_z_1, zPointsRef_interpolated{1} - zPoints{1},'DisplayName','SMC position error');
-hold on
-plot(time_z_2 - first_valid_time_z_2, zPointsRef_interpolated{2} - zPoints{2},'DisplayName','PID position error');
-hold off
-xlim([0 inf]);
-xlabel('Time[s]');
-ylabel('Distance Error[m]')
-title('Position error Z');
-legend
-
+for axis = 1:3
+    for path = 1:3
+        subplot(3, 3, path + axis*3 - 3);
+        hold off
+        for controller = 1:length(Controllers)
+            if axis == 1
+                my_title='Position error on X' + " ";
+                my_PointsRef_interpolated = xPointsRef_interpolated{controller};
+                my_Points = xPoints{controller};
+            elseif axis == 2
+                my_title='Position error on Y' + " ";
+                my_PointsRef_interpolated = yPointsRef_interpolated{controller};
+                my_Points = yPoints{controller};
+            elseif axis == 3
+                my_title='Position error on Z' + " ";
+                my_PointsRef_interpolated = zPointsRef_interpolated{controller};
+                my_Points = zPoints{controller};
+            end
+            change_1 = find(abs(diff(xPointsRef_interpolated{controller} - xPoints{controller}))>0.1);
+            change_2 = find(abs(diff(zPointsRef_interpolated{controller} - xPoints{controller}))>0.1);
+            change_1_start = change_1(1)   -2;
+            change_1_end   = change_1(end) +2;
+            change_2_start = change_2(1)   -2;
+            change_2_end   = change_2(end) +2;
+            
+            time = ptime{controller} - BAG{controller}.StartTime;
+            points = my_Points - my_PointsRef_interpolated;
+            idx1 = find(~isnan(points), 1);
+            first_valid_time = time(idx1);
+            if controller == 1
+                display_name = 'SMC with wind';
+            elseif controller == 2
+                display_name = 'PID with wind';
+            elseif controller == 3
+                display_name = 'SMC without wind';
+            elseif controller == 4
+                display_name = 'PID without wind';
+            end
+            if path == 1
+                path_mode = 'on takeoff';
+                plot(time(1:change_1_start) - first_valid_time, my_PointsRef_interpolated(1:change_1_start) - my_Points(1:change_1_start),'DisplayName',display_name );
+            elseif path == 2
+                path_mode = 'on move';
+                plot(time(change_1_end:change_2_start) - first_valid_time, my_PointsRef_interpolated(change_1_end:change_2_start) - my_Points(change_1_end:change_2_start),'DisplayName',display_name );
+            elseif path == 3
+                path_mode = 'on landing';
+                plot(time(change_2_end:end) - first_valid_time, my_PointsRef_interpolated(change_2_end:end) - my_Points(change_2_end:end),'DisplayName',display_name );
+            end
+            title(my_title + path_mode);
+            xlabel('Time[s]');
+            ylabel('Distance Error[m]')
+            legend
+            hold on
+        end
+    end
+end
 set(all_fig, 'PaperPositionMode', 'auto');
 exportgraphics(all_fig, strcat(fullfile(graph_folder, FigName), ".png"), 'Resolution', 900);
 
@@ -215,66 +212,66 @@ exportgraphics(all_fig, strcat(fullfile(graph_folder, FigName), ".png"), 'Resolu
 
 FigName = "all_fig_derivative";
 all_fig_derivative = figure('Name', FigName, 'Position', get(0, 'Screensize'));
+for axis = 1:3
+    for path = 1:3
+        subplot(3, 3, path + axis*3 - 3);
+        hold off
+        for controller = 1:length(Controllers)
+            if axis == 1
+                my_title='Absolute derivative of position error on X' + " ";
+                my_PointsRef_interpolated = xPointsRef_interpolated{controller};
+                my_Points = xPoints{controller};
+            elseif axis == 2
+                my_title='Absolute derivative of position error on Y' + " ";
+                my_PointsRef_interpolated = yPointsRef_interpolated{controller};
+                my_Points = yPoints{controller};
+            elseif axis == 3
+                my_title='Absolute derivative of position error on Z' + " ";
+                my_PointsRef_interpolated = zPointsRef_interpolated{controller};
+                my_Points = zPoints{controller};
+            end
+            change_1 = find(abs(diff(xPointsRef_interpolated{controller} - xPoints{controller}))>0.1);
+            change_2 = find(abs(diff(zPointsRef_interpolated{controller} - xPoints{controller}))>0.1);
+            change_1_start = change_1(1)   -2;
+            change_1_end   = change_1(end) +2;
+            change_2_start = change_2(1)   -2;
+            change_2_end   = change_2(end) +2;
+            
+            time = ptime{controller} - BAG{controller}.StartTime;
+            points= my_Points - my_PointsRef_interpolated;
+            idx1 = find(~isnan(points), 1);
+            first_valid_time = time(idx1);
 
-py1 = xPointsRef_interpolated{1} - xPoints{1};
-px1 = time_x_1 - first_valid_time_x_1;
-dy1=diff(py1)./diff(px1);
-py2 = xPointsRef_interpolated{2} - xPoints{2};
-px2 = time_x_2 - first_valid_time_x_2;
-dy2=diff(py2)./diff(px2);
+            py1 = my_PointsRef_interpolated - my_Points;
+            px1 = time - first_valid_time;
+            dy1=diff(py1)./diff(px1);
 
-pos_x = subplot(3, 1, 1);
-plot(px1(2:end),dy1,'DisplayName','SMC position error derivative');
-hold on
-plot(px2(2:end),dy2,'DisplayName','PID position error derivative');
-hold off
-xlim([0 inf]);
-xlabel('Time[s]');
-ylabel('Distance Error Derivative[m/s]')
-title('Position error derivative X');
-legend
-
-
-
-py1 = yPointsRef_interpolated{1} - yPoints{1};
-px1 = time_y_1 - first_valid_time_y_1;
-dy1=diff(py1)./diff(px1);
-py2 = yPointsRef_interpolated{2} - yPoints{2};
-px2 = time_y_2 - first_valid_time_y_2;
-dy2=diff(py2)./diff(px2);
-
-pos_y = subplot(3, 1, 2);
-plot(px1(2:end),dy1,'DisplayName','SMC position error derivative');
-hold on
-plot(px2(2:end),dy2,'DisplayName','PID position error derivative');
-hold off
-xlim([0 inf]);
-xlabel('Time[s]');
-ylabel('Distance Error Derivative[m/s]')
-title('Position error derivative Y');
-legend
-
-
-
-py1 = zPointsRef_interpolated{1} - zPoints{1};
-px1 = time_z_1 - first_valid_time_z_1;
-dy1=diff(py1)./diff(px1);
-py2 = zPointsRef_interpolated{2} - zPoints{2};
-px2 = time_z_2 - first_valid_time_z_2;
-dy2=diff(py2)./diff(px2);
-
-pos_z = subplot(3, 1, 3);
-plot(px1(2:end),dy1,'DisplayName','SMC position error derivative');
-hold on
-plot(px2(2:end),dy2,'DisplayName','PID position error derivative');
-hold off
-xlim([0 inf]);
-xlabel('Time[s]');
-ylabel('Distance Error Derivative[m/s]')
-title('Position error derivative Z');
-legend
-
-
-
+            if controller == 1
+                display_name = 'SMC with wind';
+            elseif controller == 2
+                display_name = 'PID with wind';
+            elseif controller == 3
+                display_name = 'SMC without wind';
+            elseif controller == 4
+                display_name = 'PID without wind';
+            end
+            if path == 1
+                path_mode = '- takeoff';
+                plot(px1(1:change_1_start),abs(dy1(1:change_1_start)),'DisplayName',display_name);
+            elseif path == 2
+                path_mode = '- move';
+                plot(px1(change_1_end:change_2_start),abs(dy1(change_1_end:change_2_start)),'DisplayName',display_name);
+            elseif path == 3
+                path_mode = '- landing';
+                plot(px1(change_2_end:end-1),abs(dy1(change_2_end:end)),'DisplayName',display_name);
+            end
+            title(my_title + path_mode);
+            xlabel('Time[s]');
+            ylabel('Distance Error Derivative[m]')
+            legend
+            hold on
+        end
+    end
+end
 set(all_fig_derivative, 'PaperPositionMode', 'auto');
 exportgraphics(all_fig_derivative, strcat(fullfile(graph_folder, FigName), ".png"), 'Resolution', 900);
