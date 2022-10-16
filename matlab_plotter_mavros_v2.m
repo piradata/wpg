349d100ab20f1ee3f9b7891820780a9f1fe8e58c
Guilherme Ferreira <guilherme.ferreira@visagio.com>
2022-10-16 18:09:07 -0300

    fix script

--- matlab_plotter_mavros_v2.m
+++ matlab_plotter_mavros_v2.m
@@ -1,4 +1,4 @@
-Controllers = ["SMC", "PID", "SMC_without_wind", "PID_without_wind"];
+Controllers = ["SMC_PID_8", "SMC_PID_7", "SMC_PID_10", "PID", "SMC_PD"];
 graph_folder = "graps";
 xPoints = {};
 yPoints = {};
@@ -30,7 +30,7 @@ for i = 1:length(Controllers)
 
     Controller = Controllers(i);
     
-    BAG{i} = rosbag(sprintf("%s_test.bag", Controller));
+    BAG{i} = rosbag(sprintf("%s.bag", Controller));
     
     titulo = sprintf("teste com controlador %s", Controller);
     
@@ -146,17 +146,17 @@ FigName = strcat("path_complete");
 path_complete_fig = figure('Name', FigName, 'Position', get(0, 'Screensize'));
 plot3(xPointsRef{1},yPointsRef{1},zPointsRef{1},'DisplayName', 'Desired movement');
 hold on
-plot3(xPoints{1},yPoints{1},zPoints{1},'DisplayName', 'Real movement SMC with wind');
-plot3(xPoints{2},yPoints{2},zPoints{2},'DisplayName', 'Real movement PID with wind');
-plot3(xPoints{3},yPoints{3},zPoints{3},'DisplayName', 'Real movement SMC without wind');
-plot3(xPoints{4},yPoints{4},zPoints{4},'DisplayName', 'Real movement PID without wind');
+for controller = 1:length(Controllers)
+    Controller = Controllers(controller);
+    plot3(xPoints{controller},yPoints{controller},zPoints{controller},'DisplayName', Controller);
+end
 title('Position tracker');
 hold off;
 
 xlabel('Absolute Position x[m]');
 ylabel('Absolute Position Y[m]')
 zlabel('Absolute Position Z[m]')
-legend('Location', 'northeast');
+legend('Location', 'northeast', 'interpreter', 'none');
 set(path_complete_fig, 'PaperPositionMode', 'auto');
 exportgraphics(path_complete_fig, strcat(fullfile(graph_folder, FigName), ".png"), 'Resolution', 900);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -168,6 +168,7 @@ for axis = 1:3
         subplot(3, 3, path + axis*3 - 3);
         hold off
         for controller = 1:length(Controllers)
+            Controller = Controllers(controller);
             if axis == 1
                 my_title='Position error on X' + " ";
                 my_PointsRef_interpolated = xPointsRef_interpolated{controller};
@@ -181,8 +182,8 @@ for axis = 1:3
                 my_PointsRef_interpolated = zPointsRef_interpolated{controller};
                 my_Points = zPoints{controller};
             end
-            change_1 = find(abs(diff(xPointsRef_interpolated{controller} - xPoints{controller}))>0.1);
-            change_2 = find(abs(diff(zPointsRef_interpolated{controller} - xPoints{controller}))>0.1);
+            change_1 = find(abs(diff(xPointsRef_interpolated{controller} - xPoints{controller}))>0.5);
+            change_2 = find(abs(diff(zPointsRef_interpolated{controller} - xPoints{controller}))>0.5);
             change_1_start = change_1(1)   -2;
             change_1_end   = change_1(end) +2;
             change_2_start = change_2(1)   -2;
@@ -192,29 +193,20 @@ for axis = 1:3
             points = my_Points - my_PointsRef_interpolated;
             idx1 = find(~isnan(points), 1);
             first_valid_time = time(idx1);
-            if controller == 1
-                display_name = 'SMC with wind';
-            elseif controller == 2
-                display_name = 'PID with wind';
-            elseif controller == 3
-                display_name = 'SMC without wind';
-            elseif controller == 4
-                display_name = 'PID without wind';
-            end
             if path == 1
                 path_mode = 'on takeoff';
-                plot(time(1:change_1_start) - first_valid_time, my_PointsRef_interpolated(1:change_1_start) - my_Points(1:change_1_start),'DisplayName',display_name );
+                plot(time(1:change_1_start) - first_valid_time, my_PointsRef_interpolated(1:change_1_start) - my_Points(1:change_1_start),'DisplayName',Controller);
             elseif path == 2
                 path_mode = 'on move';
-                plot(time(change_1_end:change_2_start) - first_valid_time, my_PointsRef_interpolated(change_1_end:change_2_start) - my_Points(change_1_end:change_2_start),'DisplayName',display_name );
+                plot(time(change_1_end:change_2_start) - first_valid_time, my_PointsRef_interpolated(change_1_end:change_2_start) - my_Points(change_1_end:change_2_start),'DisplayName',Controller);
             elseif path == 3
                 path_mode = 'on landing';
-                plot(time(change_2_end:end) - first_valid_time, my_PointsRef_interpolated(change_2_end:end) - my_Points(change_2_end:end),'DisplayName',display_name );
+                plot(time(change_2_end:end) - first_valid_time, my_PointsRef_interpolated(change_2_end:end) - my_Points(change_2_end:end),'DisplayName',Controller);
             end
             title(my_title + path_mode);
             xlabel('Time[s]');
             ylabel('Distance Error[m]')
-            legend
+            legend('interpreter', 'none')
             hold on
         end
     end
@@ -224,109 +216,109 @@ exportgraphics(all_fig, strcat(fullfile(graph_folder, FigName), ".png"), 'Resolu
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-FigName = "all_fig_derivative";
-all_fig_derivative = figure('Name', FigName, 'Position', get(0, 'Screensize'));
-for axis = 1:3
-    for path = 1:3
-        subplot(3, 3, path + axis*3 - 3);
-        hold off
-        for controller = 1:length(Controllers)
-            if axis == 1
-                my_title='Absolute derivative of position error on X' + " ";
-                my_PointsRef_interpolated = xPointsRef_interpolated{controller};
-                my_Points = xPoints{controller};
-            elseif axis == 2
-                my_title='Absolute derivative of position error on Y' + " ";
-                my_PointsRef_interpolated = yPointsRef_interpolated{controller};
-                my_Points = yPoints{controller};
-            elseif axis == 3
-                my_title='Absolute derivative of position error on Z' + " ";
-                my_PointsRef_interpolated = zPointsRef_interpolated{controller};
-                my_Points = zPoints{controller};
-            end
-            change_1 = find(abs(diff(xPointsRef_interpolated{controller} - xPoints{controller}))>0.1);
-            change_2 = find(abs(diff(zPointsRef_interpolated{controller} - xPoints{controller}))>0.1);
-            change_1_start = change_1(1)   -2;
-            change_1_end   = change_1(end) +2;
-            change_2_start = change_2(1)   -2;
-            change_2_end   = change_2(end) +2;
-            
-            time = ptime{controller} - BAG{controller}.StartTime;
-            points= my_Points - my_PointsRef_interpolated;
-            idx1 = find(~isnan(points), 1);
-            first_valid_time = time(idx1);
-
-            py1 = my_PointsRef_interpolated - my_Points;
-            px1 = time - first_valid_time;
-            dy1=diff(py1)./diff(px1);
-
-            if controller == 1
-                display_name = 'SMC with wind';
-            elseif controller == 2
-                display_name = 'PID with wind';
-            elseif controller == 3
-                display_name = 'SMC without wind';
-            elseif controller == 4
-                display_name = 'PID without wind';
-            end
-            if path == 1
-                path_mode = '- takeoff';
-                plot(px1(1:change_1_start),abs(dy1(1:change_1_start)),'DisplayName',display_name);
-            elseif path == 2
-                path_mode = '- move';
-                plot(px1(change_1_end:change_2_start),abs(dy1(change_1_end:change_2_start)),'DisplayName',display_name);
-            elseif path == 3
-                path_mode = '- landing';
-                plot(px1(change_2_end:end-1),abs(dy1(change_2_end:end)),'DisplayName',display_name);
-            end
-            title(my_title + path_mode);
-            xlabel('Time[s]');
-            ylabel('Distance Error Derivative[m]')
-            legend
-            hold on
-        end
-    end
-end
-set(all_fig_derivative, 'PaperPositionMode', 'auto');
-exportgraphics(all_fig_derivative, strcat(fullfile(graph_folder, FigName), ".png"), 'Resolution', 900);
+% FigName = "all_fig_derivative";
+% all_fig_derivative = figure('Name', FigName, 'Position', get(0, 'Screensize'));
+% for axis = 1:3
+%     for path = 1:3
+%         subplot(3, 3, path + axis*3 - 3);
+%         hold off
+%         for controller = 1:length(Controllers)
+%             if axis == 1
+%                 my_title='Absolute derivative of position error on X' + " ";
+%                 my_PointsRef_interpolated = xPointsRef_interpolated{controller};
+%                 my_Points = xPoints{controller};
+%             elseif axis == 2
+%                 my_title='Absolute derivative of position error on Y' + " ";
+%                 my_PointsRef_interpolated = yPointsRef_interpolated{controller};
+%                 my_Points = yPoints{controller};
+%             elseif axis == 3
+%                 my_title='Absolute derivative of position error on Z' + " ";
+%                 my_PointsRef_interpolated = zPointsRef_interpolated{controller};
+%                 my_Points = zPoints{controller};
+%             end
+%             change_1 = find(abs(diff(xPointsRef_interpolated{controller} - xPoints{controller}))>0.1);
+%             change_2 = find(abs(diff(zPointsRef_interpolated{controller} - xPoints{controller}))>0.1);
+%             change_1_start = change_1(1)   -2;
+%             change_1_end   = change_1(end) +2;
+%             change_2_start = change_2(1)   -2;
+%             change_2_end   = change_2(end) +2;
+%             
+%             time = ptime{controller} - BAG{controller}.StartTime;
+%             points= my_Points - my_PointsRef_interpolated;
+%             idx1 = find(~isnan(points), 1);
+%             first_valid_time = time(idx1);
+% 
+%             py1 = my_PointsRef_interpolated - my_Points;
+%             px1 = time - first_valid_time;
+%             dy1=diff(py1)./diff(px1);
+% 
+%             if controller == 1
+%                 display_name = 'SMC with wind';
+%             elseif controller == 2
+%                 display_name = 'PID with wind';
+%             elseif controller == 3
+%                 display_name = 'SMC without wind';
+%             elseif controller == 4
+%                 display_name = 'PID without wind';
+%             end
+%             if path == 1
+%                 path_mode = '- takeoff';
+%                 plot(px1(1:change_1_start),abs(dy1(1:change_1_start)),'DisplayName',display_name);
+%             elseif path == 2
+%                 path_mode = '- move';
+%                 plot(px1(change_1_end:change_2_start),abs(dy1(change_1_end:change_2_start)),'DisplayName',display_name);
+%             elseif path == 3
+%                 path_mode = '- landing';
+%                 plot(px1(change_2_end:end-1),abs(dy1(change_2_end:end)),'DisplayName',display_name);
+%             end
+%             title(my_title + path_mode);
+%             xlabel('Time[s]');
+%             ylabel('Distance Error Derivative[m]')
+%             legend
+%             hold on
+%         end
+%     end
+% end
+% set(all_fig_derivative, 'PaperPositionMode', 'auto');
+% exportgraphics(all_fig_derivative, strcat(fullfile(graph_folder, FigName), ".png"), 'Resolution', 900);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Sensor plot
-
-FigName = strcat("sensors");
-sensors_fig = figure('Name', FigName, 'Position', get(0, 'Screensize'));
-
-subplot(3,1,1)
-plot(AngAcceltime{1} - BAG{1}.StartTime,xAngAccelPoints{1},'DisplayName', 'Ang Accel X SMC with wind');
-hold on
-plot(AngAcceltime{2} - BAG{2}.StartTime,xAngAccelPoints{2},'DisplayName', 'Ang Accel X PID with wind');
-hold off
-title('angular acceleration about the FRD body frame XYZ-axis in rad/s^2');
-xlabel('Time t[s]');
-ylabel('Sensor data ang accel [rad/s^2]')
-legend('Location', 'northeast');
-
-subplot(3,1,2)
-plot(AngAcceltime{1} - BAG{1}.StartTime,yAngAccelPoints{1},'DisplayName', 'Ang Accel Y SMC with wind');
-hold on
-plot(AngAcceltime{2} - BAG{2}.StartTime,yAngAccelPoints{2},'DisplayName', 'Ang Accel Y PID with wind');
-hold off
-title('angular acceleration about the FRD body frame XYZ-axis in rad/s^2');
-xlabel('Time t[s]');
-ylabel('Sensor data ang accel [rad/s^2]')
-legend('Location', 'northeast');
-
-subplot(3,1,3)
-plot(AngAcceltime{1} - BAG{1}.StartTime,zAngAccelPoints{1},'DisplayName', 'Ang Accel Z SMC with wind');
-hold on
-plot(AngAcceltime{2} - BAG{2}.StartTime,zAngAccelPoints{2},'DisplayName', 'Ang Accel Z PID with wind');
-hold off
-title('angular acceleration about the FRD body frame XYZ-axis');
-xlabel('Time t[s]');
-ylabel('Sensor data ang accel [rad/s^2]')
-legend('Location', 'northeast');
-
-
-set(sensors_fig, 'PaperPositionMode', 'auto');
-exportgraphics(sensors_fig, strcat(fullfile(graph_folder, FigName), ".png"), 'Resolution', 900);
+% 
+% FigName = strcat("sensors");
+% sensors_fig = figure('Name', FigName, 'Position', get(0, 'Screensize'));
+% 
+% subplot(3,1,1)
+% plot(AngAcceltime{1} - BAG{1}.StartTime,xAngAccelPoints{1},'DisplayName', 'Ang Accel X SMC with wind');
+% hold on
+% plot(AngAcceltime{2} - BAG{2}.StartTime,xAngAccelPoints{2},'DisplayName', 'Ang Accel X PID with wind');
+% hold off
+% title('angular acceleration about the FRD body frame XYZ-axis in rad/s^2');
+% xlabel('Time t[s]');
+% ylabel('Sensor data ang accel [rad/s^2]')
+% legend('Location', 'northeast');
+% 
+% subplot(3,1,2)
+% plot(AngAcceltime{1} - BAG{1}.StartTime,yAngAccelPoints{1},'DisplayName', 'Ang Accel Y SMC with wind');
+% hold on
+% plot(AngAcceltime{2} - BAG{2}.StartTime,yAngAccelPoints{2},'DisplayName', 'Ang Accel Y PID with wind');
+% hold off
+% title('angular acceleration about the FRD body frame XYZ-axis in rad/s^2');
+% xlabel('Time t[s]');
+% ylabel('Sensor data ang accel [rad/s^2]')
+% legend('Location', 'northeast');
+% 
+% subplot(3,1,3)
+% plot(AngAcceltime{1} - BAG{1}.StartTime,zAngAccelPoints{1},'DisplayName', 'Ang Accel Z SMC with wind');
+% hold on
+% plot(AngAcceltime{2} - BAG{2}.StartTime,zAngAccelPoints{2},'DisplayName', 'Ang Accel Z PID with wind');
+% hold off
+% title('angular acceleration about the FRD body frame XYZ-axis');
+% xlabel('Time t[s]');
+% ylabel('Sensor data ang accel [rad/s^2]')
+% legend('Location', 'northeast');
+% 
+% 
+% set(sensors_fig, 'PaperPositionMode', 'auto');
+% exportgraphics(sensors_fig, strcat(fullfile(graph_folder, FigName), ".png"), 'Resolution', 900);
 
